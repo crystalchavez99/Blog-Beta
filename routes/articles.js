e379f79cb5606ad9b02f6609113f3a9b14b5abda
Crystal <crystal120599@gmail.com>
2022-03-08 10:41:12 -0800

    Delete works

--- routes/articles.js
+++ routes/articles.js
@@ -4,6 +4,11 @@ const {Article} = require("../models");
 router.get('/new',(req,res)=>{
     res.render('articles/new', {article: new Article() })
 })
+
+router.get(`/edit/:id`,async(req,res)=>{
+    const article = await Article.findByPk(req.params.id)
+    res.render('articles/edit', {article: new Article() })
+})
 router.get(`/:id`, async(req,res)=>{
     const article = await Article.findByPk(req.params.id);
     if(article === null){
@@ -24,16 +29,12 @@ router.post('/', async(req,res)=>{
         console.log(e)
         res.render('articles/new',{article: article})
     }
-
 })
 
+
 router.delete(`/:id`, async(req,res)=>{
-    const article = await Article.findByPk(req.params.id);
-    await Article.destory({
-        where:{
-            id: article.id
-        }
-    })
+    const artId = await Article.findByPk(req.params.id);
+    await artId.destroy();
     res.redirect("/");
 })
 
